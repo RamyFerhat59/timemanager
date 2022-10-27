8762740dad653d371ab02beedc516afc762e2af8
tci <thomas.cingoz@epitech.eu>
2022-10-27 14:29:53 +0200

    [ADD] : frontend app in vuejs

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import User from './components/User.vue'
+import WorkingTimes from './components/WorkingTimes.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <User msg="Test" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/User.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        email: ''
+      },
+      user: {
+        name: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    checkUser() {
+      fetch('http://localhost:3000/api/users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        // if (data.name === null) {
+        //   createUser()
+        // } else {
+        //   getUser()
+        // }
+      })
+    }
+  }
+}
+
+function getUser() {
+  fetch('http://localhost:3000/api/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    this.user.name = data.name
+    this.user.email = data.email
+  })
+}
+
+function createUser() {
+  fetch('http://localhost:3000/api/users', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      user: {
+        name: this.user.name,
+        email: this.user.email
+      }
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    this.user.name = data.name
+    this.user.email = data.email
+  })
+}
+
+</script>
+
+<template>
+  <div class="profile">
+    <h1>Log in:</h1>
+  </div>
+  <div class="login">
+    <span><input v-model="form.name" placeholder="Username" /></span>
+    <span><input v-model="form.email" placeholder="Email" /></span>
+    <span><button v-on:click="checkUser">Log in</button></span>
+  </div>
+</template>
