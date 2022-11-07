c65ce8d406f69578a59c73d7d2f2e2755e812181
RamyFerhat59 <ramy.ferhat@epitech.eu>
2022-11-07 12:40:19 +0100

    test¨

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker --version
 docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
 docker-compose -f docker-compose.build.yml build
