e71d6641ce3664f00522dac45e08739a0860839d
RamyFerhat59 <ramy.ferhat@epitech.eu>
2022-11-07 15:02:25 +0100

    get url

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker --version
 docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 sudo curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sudo sh
 sudo docker-compose -f docker-compose.build.yml build
@@ -8,4 +7,5 @@ sudo docker-compose -f docker-compose.build.yml push
 sudo docker context create ecs deploy-phoenix --from-env
 sudo docker context use deploy-phoenix
 sudo docker compose -f docker-compose.prod.yml up
+sudo docker compose -f docker-compose.prod.yml ps
 
